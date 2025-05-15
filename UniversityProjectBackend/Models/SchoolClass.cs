1d6ae322e06b0f67370e8b3eff47bd844affa21b
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-05-15 14:32:22 +0200

    osztályok

---
+++ UniversityProjectBackend/Models/SchoolClass.cs
@@ -0,0 +1,23 @@
+﻿namespace UniversityProjectBackend.Models
+{
+    public class SchoolClass
+    {
+        public SchoolClass(int id, string classId)
+        {
+            if(string.IsNullOrEmpty(classId))
+            {
+                throw new ArgumentException("ClassId nem lehet üres vagy null", nameof(classId));
+            }
+            Id = id;
+            ClassId = classId;
+        }
+
+        public int Id { get; set; }
+        public string ClassId { get; set; }
+
+        public override string ToString()
+        {
+            return $"Osztály idje: {Id} Osztály megnevezése: {ClassId}";
+        }
+    }
+}
