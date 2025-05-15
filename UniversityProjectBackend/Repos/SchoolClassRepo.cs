212c2ed9780a96f2710be8fbf8bbb4d481bb3988
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-05-15 14:36:49 +0200

    dotnet ef dolgok + repo létrehozva

---
+++ UniversityProjectBackend/Repos/SchoolClassRepo.cs
@@ -0,0 +1,22 @@
+﻿using UniversityProjectBackend.Models;
+
+namespace UniversityProjectBackend.Repos
+{
+    public class SchoolClassRepo
+    {
+        private List<SchoolClass> _schoolClasses = new List<SchoolClass>();
+        public void Add(SchoolClass schoolClass)
+        {
+            if (schoolClass == null)
+            {
+                throw new ArgumentNullException(nameof(schoolClass), "Iskolai osztály nem lehet null");
+            }
+            _schoolClasses.Add(schoolClass);
+        }
+        public List<SchoolClass> GetAllClasses()
+        {
+            return _schoolClasses;
+        }
+        
+    }
+}
